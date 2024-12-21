9c186d9989b8767758866367b0be332b30179ed6
blanc <chadahaa@gmail.com>
2024-12-21 11:34:24 +0700

    add styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image';
+import styles from './Home.module.css';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <img src="/cat.jpg" alt="" width="200" height="200" />
-    </div>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Welcome to My App</h1>
+        <p className={styles.description}>Discover the beauty of simplicity</p>
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/cat.jpg"
+            alt="Cute cat"
+            width={300}
+            height={300}
+            className={styles.image}
+          />
+        </div>
+        <button className={styles.button}>
+          <a href="https://www.google.com" target="_blank">Get Started</a>
+          </button>
+      </div>
+    </main>
   );
 }
